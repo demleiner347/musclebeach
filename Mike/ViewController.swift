4044f9136c47a88acbfefcb9731a2ba61008dae9
Unknown <mike.demlein@gmail.com>
2017-10-12 10:59:56 -0400

    We did this is GitKraken

--- Mike/ViewController.swift
+++ Mike/ViewController.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Michael Demlein on 10/10/17.
 //  Copyright © 2017 Michael Demlein. All rights reserved.
-//
+//  Muscle Beach rules
 
 import UIKit
 
@@ -19,9 +19,9 @@ class ViewController: UIViewController {
         buttonCount = buttonCount + 1
         print (buttonCount)
         
-        if buttonCount >= 15 { view.backgroundColor = UIColor.green
+        if buttonCount >= 5 { view.backgroundColor = UIColor.blue
             
-            myLabel.text = "You WIN!!"
+            myLabel.text = "Eat a dick"
         }
         
         

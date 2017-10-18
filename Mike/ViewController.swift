43ec167aea7e7fc9f8085a2d851a155c529b85f5
Unknown <mike.demlein@gmail.com>
2017-10-18 12:35:19 -0400

    Adding booleans!

--- Mike/ViewController.swift
+++ Mike/ViewController.swift
@@ -15,11 +15,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     
+    @IBOutlet weak var additionSwitch: UISwitch!
     
     @IBAction func buttonTapped(_ sender: Any) {
         
         print(topTextField.text!)
         print(bottomTextField.text!)
+        
+        let addition = additionSwitch.isOn
+        
+        if addition {let sum = Double(topTextField.text!)! + Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) + \(bottomTextField.text!) = \(sum)"
+            
+        } else {let sum = Double(topTextField.text!)! - Double(bottomTextField.text!)!
+            
+            myLabel.text = "\(topTextField.text!) - \(bottomTextField.text!) = \(sum)"
+            
+        }
+            
+        
         
         
         
